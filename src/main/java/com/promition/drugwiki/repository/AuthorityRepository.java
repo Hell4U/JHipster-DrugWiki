cff7309d25a6566bda19bae191744b7b4bc5cdfd
Hell4U <18BCE131@nirmauni.ac.in>
2021-06-03 18:51:51 +0530

    Initial version of DrugWiki generated by JHipster-7.0.1

---
+++ src/main/java/com/promition/drugwiki/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.promition.drugwiki.repository;
+
+import com.promition.drugwiki.domain.Authority;
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
+
+/**
+ * Spring Data R2DBC repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends R2dbcRepository<Authority, String> {}
