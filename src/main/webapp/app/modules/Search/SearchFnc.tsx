0f3d128f45852a974512b42a703fa1d2e28ae5a7
Hell4U <18BCE131@nirmauni.ac.in>
2021-06-25 17:51:21 +0530

    Nothing new

--- src/main/webapp/app/modules/Search/SearchFnc.tsx
+++ src/main/webapp/app/modules/Search/SearchFnc.tsx
@@ -1,45 +1,80 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { InputGroup, InputGroupAddon, InputGroupText, Input } from 'reactstrap';
 import { Button } from 'reactstrap';
+import { ListGroup, ListGroupItem } from 'reactstrap';
+import styles from './SearchFnc.module.scss';
 
 const tmpData = [
   {
     id: 1,
-    name: "TeraByte"
+    name: 'TeraByte',
   },
   {
     id: 2,
-    name: "ZotaByte"
+    name: 'ZotaByte',
   },
   {
     id: 3,
-    name:"PetaByte"
+    name: 'PetaByte',
   },
   {
     id: 4,
-    name: "Tera kya hoga"
-  }
-]
+    name: 'Giga Hepta Byte',
+  },
+];
+let searchListRender = null;
 
 const SearchFnc = props => {
-  const searchHandler = event => {
-    if (event.type === 'click' || event.keyCode === 13) {
-    }
+  const [searchList, setSearchList] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const searchButtonHandler = event => {
+    console.log(event.target);
+  };
+
+  const searchEnterHandler = event => {
+    if (event.keyCode === 13) console.log(event.target);
+  };
+
+  const searchListHandler = (id, e) => {
+    console.log(id);
   };
 
   const searchInputHandler = event => {
-    if (event.target.value.length >= 3)
-      
+    if (event.target.value.length >= 3) {
+      setSearchList(state => [...tmpData]);
+      setSearchTerm(event.target.value);
+    } else {
+      setSearchList([]);
+      setSearchTerm('');
+    }
   };
+
+  useEffect(() => {
+    searchListRender = (
+      <ListGroup className={styles['search-list-group']}>
+        {searchList
+          .filter(data => {
+            if (data.name.toLowerCase().includes(searchTerm.toLowerCase())) return data;
+          })
+          .map(data => (
+            <ListGroupItem className={styles['search-list']} key={data.id} onClick={searchListHandler.bind(this, data.id)}>
+              {data.name}
+            </ListGroupItem>
+          ))}
+      </ListGroup>
+    );
+  }, [searchTerm]);
 
   return (
     <div>
-      <InputGroup className="width-100" style={{ width: '32rem' }}>
-        <Input placeholder="Search...." onChange={searchInputHandler} onKeyDown={searchHandler} />
-        <Button color="primary" className="ml-1" onClick={searchHandler}>
+      <InputGroup style={{ width: '32rem' }}>
+        <Input className={styles['search-input']} placeholder="Search...." onChange={searchInputHandler} onKeyDown={searchEnterHandler} />
+        <Button color="primary" className="ml-1" onClick={searchButtonHandler}>
           Search
         </Button>
       </InputGroup>
+      {searchListRender}
     </div>
   );
 };
--- src/main/webapp/app/modules/home/HomeNew.tsx
+++ src/main/webapp/app/modules/home/HomeNew.tsx
@@ -1,64 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { connect } from 'react-redux';
-import { Row, Col } from 'reactstrap';
-import { Card, CardBody, CardTitle, CardSubtitle, CardText, CardImg } from 'reactstrap';
-import { getEntities } from '../../entities/brand/brand.reducer';
-import { IRootState } from 'app/shared/reducers';
-import Capsule from 'app/Images/capsule.png';
-import Injection from 'app/Images/injection.png';
-import Tablet from 'app/Images/tablet.png';
-import Syrup from 'app/Images/syrup.png';
-
-const imageType = {
-  Capsule: Capsule,
-  Injection: Injection,
-  Tablet: Tablet,
-  Syrup: Syrup,
-};
+import BrandShower from '../ShowEntity/Brand/BrandShower';
 
 const HomeNew = props => {
-  const { brandList } = props;
-
-  useEffect(() => {
-    props.getEntities();
-  }, []);
-
   return (
     <>
-      <Row className="mt-5">
-        {brandList && brandList.length > 0
-          ? brandList.map((elm, idx) => {
-              return (
-                <Col md="2" className="mb-5" key={elm.id}>
-                  <Card>
-                    <CardImg top width="300px" height="150px" alt="Type" src={imageType[elm.type]} />
-                    <CardBody>
-                      <CardTitle tag="h4">{elm.bname}</CardTitle>
-                      <CardSubtitle tag="h6" className="mb-4 mt-0 text-muted">
-                        {elm.companyofMedicine ? elm.companyofMedicine.cname : ''}
-                      </CardSubtitle>
-                      <CardText>Price:-{elm.price}</CardText>
-                    </CardBody>
-                  </Card>
-                </Col>
-              );
-            })
-          : null}
-      </Row>
+      <BrandShower />
     </>
   );
 };
 
-const mapStateToProps = ({ brand }: IRootState) => ({
-  brandList: brand.entities,
-  loading: brand.loading,
-});
-
-const mapDispatchToProps = {
-  getEntities,
-};
-
-type StateProps = ReturnType<typeof mapStateToProps>;
-type DispatchProps = typeof mapDispatchToProps;
-
-export default connect(mapStateToProps, mapDispatchToProps)(HomeNew);
+export default HomeNew;
